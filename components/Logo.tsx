f3591406baff496cc673d0e980ce35d8a950fe21
eleo95 <e.leonardo7474@gmail.com>
2024-04-27 21:16:12 -0400

    feat: auth setup

---
+++ components/Logo.tsx
@@ -0,0 +1,12 @@
+import { HandCoins } from "lucide-react";
+
+export default function Logo() {
+  return (
+    <a href="/" className="flex items-center gap-2">
+      <HandCoins className="stroke h-11 w-11 stroke-lime-500 stroke-[1.5]" />
+      <p className="bg-gradient-to-r from-lime-400 to-green-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        Expenshee
+      </p>
+    </a>
+  );
+}
