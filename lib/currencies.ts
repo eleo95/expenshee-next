83611c7f7608153abdabce0b91708404b5a7e5ab
eleo95 <e.leonardo7474@gmail.com>
2024-04-29 10:58:16 -0400

    chore: currency selector handler

--- lib/currencies.ts
+++ lib/currencies.ts
@@ -1,7 +1,7 @@
 export const Currencies = [
     {value: "USD", label:"$ Dollar", locale:"en-US"},
     {value: "EUR", label:"€ Euro", locale:"es-ES"},
-    {value: "DOP", label:"$ Peso", locale:"es-DO"},
+    {value: "DOP", label:"$ Dominican Peso", locale:"es-DO"},
     {value: "GBP", label:"£ Pound", locale:"en-GB"},
     {value: "JPY", label:"¥ Yen", locale:"ja-JP"},
 ]
---
+++ schema/userSettings.ts
@@ -0,0 +1,12 @@
+import { Currencies } from "@/lib/currencies";
+import { z } from "zod";
+
+export const UpdateUserCurrencySchema = z.object({
+  currency: z.custom((value) => {
+    const found = Currencies.some((e) => e.value === value);
+    if (!found) {
+      throw new Error(`Invalid currency: ${value}`);
+    }
+    return value;
+  }),
+});
